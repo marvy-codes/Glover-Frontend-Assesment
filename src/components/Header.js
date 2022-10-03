2198a8b2c176d1b1cb41c339ce1c9da2323d857b
marvyCodes <olowomarve@gmail.com>
2022-10-03 23:26:49 +0100

    add text

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import icon from "../assets/icon.png"
 function Header() {
   return (
     <div className="max-w-screen-2xl mx-4 sm:mx-8 md:mx-12 2xl:px-[158px] 2xl:mx-auto  mt-11">
-      <div>
+      <div className="flex spaced-between">
         <h1 className="pl-12">Events</h1>
         <div>
           <img src={icon} alt='alt' />
